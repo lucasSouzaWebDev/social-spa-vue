eb9fb9bbd42f3627b21468ddde323a8e0be3ce75
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-12-22 10:45:43 -0300

    Adicionado Vuex

--- src/main.js
+++ src/main.js
@@ -5,15 +5,38 @@ import './plugins/axios'
 import App from './App'
 import router from './router'
 import axios from "axios"
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.prototype.$urlAPI = 'http://127.0.0.1:8000/api/'
 
+var store = {
+  state: {
+    nome: 'Lucas',
+    sobrenome: 'de Souza'
+  },
+  getters: {
+    getNome: state => {
+      return state.nome;
+    },
+    getNomeCompleto: state => {
+      return state.nome + ' ' + state.sobrenome;
+    }
+  },
+  mutations: {
+    setNome(state, nome) {
+      state.nome = nome;
+    }
+  }
+}
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: new Vuex.Store(store),
   router,
   components: { App },
   template: '<App/>'
